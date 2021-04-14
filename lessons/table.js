7acd43baeb265efbc3e01aff5a8c4678c5526efc
philippe sanogo <philippe.sanogo@gmail.com>
2021-04-15 00:58:17 +0200

    mis à jour du contenu dans table.js

--- lessons/table.js
+++ lessons/table.js
@@ -1,31 +1,43 @@
 /**
- * Les tableaux
+ * La gestion des tableaux
  */
 
 
-let tableauDeNombre = [1,2,3,4,7];
-console.log(tableauDeNombre);
 
-// mettre à jour un élément du tableau
-tableauDeNombre[4] = 5;
-console.log(tableauDeNombre);
 
-// ajouter un élément à la fin du tableau
-tableauDeNombre.push(6);
-console.log(tableauDeNombre);
+// exemple de gestion d'un tableau:
+console.log(`Exemple de gestion d'un tableau:`);
 
-// retirer un élément à la fin du tableau
-tableauDeNombre.pop();
-console.log(tableauDeNombre);
+// déclaration et assignation du tableau
+let tab = ["hello", 45, true, 12.5];
 
-// ajouter un élément au début du tableau
-tableauDeNombre.unshift(0);
-console.log(tableauDeNombre);
+// retourner le tableau
+tab.reverse();
 
-// retirer un élément au début du tableau
-tableauDeNombre.shift();
-console.log(tableauDeNombre);
+// ajoute 12 au debut du tableau
+tab.unshift(12);
 
-// afficher la longueur du tableau
-tableauDeNombre.length;
-console.log(tableauDeNombre.length);
+// ajoute au revoir et merci à la fin du tableau
+tab.push("au revoir", "merci");
+
+// enlever merci
+tab.pop();
+
+// enlever 12
+tab.shift();
+
+// ajouter "false" en position 2 sur le tableau
+tab.splice(2, 0, false);
+
+// remplacer "hello" par "hi"
+tab.splice(4, 1, "hi");
+
+// affiche le tableau sous forme de tableau
+console.table(tab);
+
+// affiche le tableau dans la console
+console.log(tab);
+
+// affiche la longueur du tableau
+let longueurTab = tab.length;
+console.log(longueurTab);
